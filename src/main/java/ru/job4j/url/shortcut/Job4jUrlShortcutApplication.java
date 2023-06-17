0fa6f3bfc5c65d989b1f220a84e92104b38a0ba6
PavelM2205 <pavelm2205@mail.ru>
2023-06-17 14:42:55 +0700

    Добавлен checkstyle

--- src/main/java/ru/job4j/url/shortcut/Job4jUrlShortcutApplication.java
+++ src/main/java/ru/job4j/url/shortcut/Job4jUrlShortcutApplication.java
@@ -1,4 +1,4 @@
-package ru.job4j.url_shortcut;
+package ru.job4j.url.shortcut;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
